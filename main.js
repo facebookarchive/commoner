b2e5e9e7c8c43751976a9a5d7c437b58554d2c03
Ben Newman <bn@cs.stanford.edu>
2013-11-06 17:04:36 -0500

    Allow Commoner script authors to specify a custom version.

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 var path = require("path");
-var util = require("./lib/util");
-
-var versionP = util.readJsonFileP(
-    path.join(__dirname, "package.json")
-).get("version");
-
 var Commoner = require("./lib/commoner").Commoner;
 exports.Commoner = Commoner;
 
 function defCallback(name) {
     exports[name] = function() {
         var commoner = new Commoner;
-
-        versionP.then(function(version) {
-            commoner.cliBuildP(version);
-        });
-
-        return commoner[name].apply(commoner, arguments);
+        commoner[name].apply(commoner, arguments);
+        commoner.cliBuildP();
+        return commoner;
     };
 }
+
+defCallback("version");
 defCallback("resolve");
 defCallback("process");
